0575c131011e4bef63154271e3623b9f415b0497
1789146079 <1789146079@qq.com>
2017-11-30 15:44:03 +0800

    1111

--- consumer/ src/test/java/com/qp/test/test.java
+++ consumer/ src/test/java/com/qp/test/test.java
@@ -10,11 +10,17 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ClassPathXmlApplicationContext app=new ClassPathXmlApplicationContext("applicationContext.xml");
-		app.start();
+
+
+		
+		System.out.println(11111);
+		
 		int A=111;
 		System.out.println(A);
 		
+		int y =1111;
+		ClassPathXmlApplicationContext app=new ClassPathXmlApplicationContext("applicationContext.xml");
+		app.start();
 	}
 
 }
